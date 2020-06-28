d012ef643bd8f3a59d6b8819987df1eb3ce81731
masewo <masewo@gmx.net>
2020-06-28 21:24:28 +0200

    workaround for places and directions api on web

--- lib/google_maps/google_maps_client.dart
+++ lib/google_maps/google_maps_client.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart'
     as google_maps_flutter;
 import 'package:google_maps_webservice/directions.dart';
@@ -7,8 +8,16 @@ import 'package:google_maps_webservice/places.dart';
 class GoogleMapsClient {
   static final String apiKey = "xxx";
 
-  final _places = GoogleMapsPlaces(apiKey: apiKey);
-  final _directions = GoogleMapsDirections(apiKey: apiKey);
+  final _places = GoogleMapsPlaces(
+      apiKey: apiKey,
+      baseUrl: kIsWeb
+          ? 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api'
+          : null);
+  final _directions = GoogleMapsDirections(
+      apiKey: apiKey,
+      baseUrl: kIsWeb
+          ? 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api'
+          : null);
 
   Future<List<Prediction>> autocomplete(String input) async {
     var response = await _places.autocomplete(input,
@@ -18,8 +27,10 @@ class GoogleMapsClient {
     return response.predictions;
   }
 
-  Future<DirectionsResponse> directionsWithAddress(String origin,
-      String destination, {List<google_maps_flutter.LatLng> waypoints, TravelMode travelMode = TravelMode.driving}) async {
+  Future<DirectionsResponse> directionsWithAddress(
+      String origin, String destination,
+      {List<google_maps_flutter.LatLng> waypoints,
+      TravelMode travelMode = TravelMode.driving}) async {
     var response = await _directions.directionsWithAddress(
       origin,
       destination,
